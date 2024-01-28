5bce8eb0578abccb0c68e67697c2805857713663
Blakscorpion <Blakscorpion@users.noreply.github.com>
2024-01-28 15:12:24 +0100

    fix input weapon when scale 0
    fix input weapon when scale 0

--- Assets/Thomas/S_InputManager.cs
+++ Assets/Thomas/S_InputManager.cs
@@ -7,7 +7,7 @@ public class S_InputManager : MonoBehaviour
     public GameObject banana;
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && Time.timeScale != 0)
         {
             Vector3 mousePosition = Input.mousePosition;
             Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
