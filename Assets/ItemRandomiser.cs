6041b57d068f6eda9ca8c4e2687b64d1501636f1
Blakscorpion <Blakscorpion@users.noreply.github.com>
2024-01-28 16:26:06 +0100

    updating level with materials/textures
    updating level with materials/textures

--- Assets/ItemRandomiser.cs
+++ Assets/ItemRandomiser.cs
@@ -19,14 +19,9 @@ public class ItemRandomiser : MonoBehaviour
     void Start()
     {
         SpriteLeft.GetComponent<SpriteRenderer>().sprite = listSpriteLeft[Random.Range(0, listSpriteLeft.Length)];
-        Debug.Log(Random.Range(0, listSpriteLeft.Length));
         SpriteDesk.GetComponent<SpriteRenderer>().sprite = listSpriteDesk[Random.Range(0, listSpriteDesk.Length)];
-        Debug.Log(Random.Range(0, listSpriteDesk.Length));
         SpriteTopDesk.GetComponent<SpriteRenderer>().sprite = listSpriteTopDesk[Random.Range(0, listSpriteTopDesk.Length)];
-        Debug.Log(Random.Range(0, listSpriteTopDesk.Length));
         SpriteWindow.GetComponent<SpriteRenderer>().sprite = listSpriteWindow[Random.Range(0, listSpriteWindow.Length)];
-        Debug.Log(Random.Range(0, listSpriteWindow.Length));
         SpriteRight.GetComponent<SpriteRenderer>().sprite = listSpriteRight[Random.Range(0, listSpriteRight.Length)];
-        Debug.Log(Random.Range(0, listSpriteRight.Length));
     }
 }
