fce43563561307c499ecdbf952657c50d6ed0988
Installshi3ld <thomas.wawrysko@gmail.com>
2024-01-28 09:55:26 +0100

    Banana Anim

--- Assets/Thomas/S_BaseItem.cs
+++ Assets/Thomas/S_BaseItem.cs
@@ -6,16 +6,29 @@ public abstract class S_BaseItem : MonoBehaviour
 {
     public float force;
     public Vector3 direction;
+
+    public Renderer renderer;
+    public Texture interactedImage;
+    float _itemDeathFloor = 1000f;
+
+
     public abstract void Interact(GameObject entity);
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "npc")
         {
+            Rigidbody2D collision2D = gameObject.GetComponent<Rigidbody2D>();
+            renderer.material.mainTexture = interactedImage;
+
+            collision2D.AddTorque(Random.Range(350, 600));
+            collision2D.AddForce(new Vector2(Random.Range(-100000, 100000), Random.Range(100000, 300000)));
+
             this.gameObject.GetComponent<Collider2D>().enabled = false;
             Invoke(nameof(WaitSecond), 3.0f);
         }
     }
+
 
     void WaitSecond()
     {
