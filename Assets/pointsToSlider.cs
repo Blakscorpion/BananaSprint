a343714f9598d66943660c56e7286aedba31904b
Blakscorpion <Blakscorpion@users.noreply.github.com>
2024-01-28 15:06:43 +0100

    main ui slider points
    main ui slider points

---
+++ Assets/pointsToSlider.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pointsToSlider : MonoBehaviour
+{
+    Slider _slider;
+    float currentPoints;
+    float currentSliderValue;
+    public float pointsBeforeNextWeapon=1000f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _slider = GetComponent<Slider>();
+        currentPoints = 0f;
+        currentSliderValue = 0f;
+    }
+
+    void Update()
+    {
+        currentPoints = ScoringManager.instance.currentScore;
+        currentSliderValue = currentPoints / pointsBeforeNextWeapon;
+        _slider.value = currentSliderValue;
+    }
+
+    private void EffectWhenMaxPoints()
+    {
+        if (currentPoints >= pointsBeforeNextWeapon)
+        {
+            // Give new weapon in the list of weapons
+        }
+    }
+}
